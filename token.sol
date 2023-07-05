4443b244f1efd1f541d66fff09395db71bf05bc5
Sarthak Bhatt <sarthakbhatt0@gmail.com>
2023-07-05 15:54:15 +0000

    up

--- token.sol
+++ token.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
-// pragma solidity >=0.5.0 < 0.9.0;
+
+pragma solidity >=0.5.0 < 0.9.0;
+// pragma solidity 0.8.18;
 
 /*
        REQUIREMENTS
